a6e269e8c2f2d8d3db377b4b0f7ad99525e8f23d
Guilherme Silva <sgsouza.173@gmail.com>
2024-10-16 11:51:10 -0300

    create controller

--- src/main/java/dev/gui/data_fy/DataFyApplication.java
+++ src/main/java/dev/gui/data_fy/DataFyApplication.java
@@ -2,8 +2,10 @@ package dev.gui.data_fy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DataFyApplication {
 
 	public static void main(String[] args) {
