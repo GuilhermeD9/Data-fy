316bc52b4a8ab04f19af680b55e0051c12b4f4e8
Guilherme Silva <sgsouza.173@gmail.com>
2024-10-16 11:35:49 -0300

    Adicionando o projeto Spring

---
+++ src/main/java/dev/gui/spot_enjoy/SpotEnjoyApplication.java
@@ -0,0 +1,13 @@
+package dev.gui.spot_enjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotEnjoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotEnjoyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gui/spot_enjoy/SpotEnjoyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gui.spot_enjoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotEnjoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
