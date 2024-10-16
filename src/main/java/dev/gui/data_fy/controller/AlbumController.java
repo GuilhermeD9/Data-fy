fe585ee1804a8f126d8b4a5cd09d7a6fe4d5807e
Guilherme Silva <sgsouza.173@gmail.com>
2024-10-16 11:55:24 -0300

    create client

--- src/main/java/dev/gui/data_fy/controller/AlbumController.java
+++ src/main/java/dev/gui/data_fy/controller/AlbumController.java
@@ -1,5 +1,6 @@
 package dev.gui.data_fy.controller;
 
+import dev.gui.data_fy.client.GoogleClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/spotify/api")
 public class AlbumController {
 
+    private final GoogleClient googleClient;
+
+    public AlbumController(GoogleClient googleClient) {
+        this.googleClient = googleClient;
+    }
+
     @GetMapping("/albums")
     public ResponseEntity<String> helloWorld() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(googleClient.helloWorld());
     }
 }
