21ae582d47274308494b770f51c854f65a698737
Guilherme Silva <sgsouza.173@gmail.com>
2024-10-17 11:01:49 -0300

    Finalização da camada service e correção do controller

--- src/main/java/dev/gui/data_fy/controller/AlbumController.java
+++ src/main/java/dev/gui/data_fy/controller/AlbumController.java
@@ -3,6 +3,7 @@ package dev.gui.data_fy.controller;
 import dev.gui.data_fy.client.*;
 import dev.gui.data_fy.model.Album;
 import dev.gui.data_fy.model.LoginRequest;
+import dev.gui.data_fy.service.SpotifyService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,24 +14,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/spotify/api")
 public class AlbumController {
+    private final SpotifyService spotifyService;
 
-    private final AuthSpotifyClient authSpotifyClient;
-
-    private final AlbumSpotifyClient albumSpotifyClient;
-
-    public AlbumController(AuthSpotifyClient authSpotifyClient, AlbumSpotifyClient albumSpotifyClient) {
-        this.authSpotifyClient = authSpotifyClient;
-        this.albumSpotifyClient = albumSpotifyClient;
+    public AlbumController(SpotifyService spotifyService) {
+        this.spotifyService = spotifyService;
     }
 
     @GetMapping("/albums")
-    public ResponseEntity<List<Album>> helloWorld() {
-        var request = new LoginRequest("client_credentials",
-                "e7b49745a69b45059d31033e2638e8c1",
+    public ResponseEntity<List<Album>> getNewAlbums() {
+        List<Album> albums = spotifyService.getNewReleases("e7b49745a69b45059d31033e2638e8c1",
                 "1de1a2bc971941218785ddc0ca320c2d");
-        var token = authSpotifyClient.login(request).getAccessToken();
-
-        var response =albumSpotifyClient.getReleases("Bearer " + token);
-        return ResponseEntity.ok(response.getAlbums().getItems());
+        return ResponseEntity.ok(albums);
     }
 }
--- src/main/java/dev/gui/data_fy/service/SpotifyService.java
+++ src/main/java/dev/gui/data_fy/service/SpotifyService.java
@@ -1,4 +1,27 @@
 package dev.gui.data_fy.service;
 
+import dev.gui.data_fy.client.AlbumSpotifyClient;
+import dev.gui.data_fy.client.AuthSpotifyClient;
+import dev.gui.data_fy.model.Album;
+import dev.gui.data_fy.model.LoginRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SpotifyService {
+    private final AuthSpotifyClient authSpotifyClient;
+    private final AlbumSpotifyClient albumSpotifyClient;
+
+    public SpotifyService(AuthSpotifyClient authSpotifyClient, AlbumSpotifyClient albumSpotifyClient) {
+        this.authSpotifyClient = authSpotifyClient;
+        this.albumSpotifyClient = albumSpotifyClient;
+    }
+
+    public List<Album> getNewReleases(String clientId, String clientSecret) {
+        var request = new LoginRequest("client_credentials", clientId, clientSecret);
+        var token = authSpotifyClient.login(request).getAccessToken();
+        var response = albumSpotifyClient.getReleases("Bearer " + token);
+        return response.getAlbums().getItems();
+    }
 }
