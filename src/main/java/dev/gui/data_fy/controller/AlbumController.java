bce2239b39f531789b62a73b2ca737218921ca2f
Guilherme Silva <sgsouza.173@gmail.com>
2024-10-17 10:36:10 -0300

    Api funcionando! Finalização da implementação

--- src/main/java/dev/gui/data_fy/controller/AlbumController.java
+++ src/main/java/dev/gui/data_fy/controller/AlbumController.java
@@ -1,12 +1,12 @@
 package dev.gui.data_fy.controller;
 
-import dev.gui.data_fy.client.AuthSpotifyClient;
-import dev.gui.data_fy.client.GoogleClient;
-import dev.gui.data_fy.client.LoginRequest;
+import dev.gui.data_fy.client.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/spotify/api")
@@ -14,15 +14,21 @@ public class AlbumController {
 
     private final AuthSpotifyClient authSpotifyClient;
 
-    public AlbumController(AuthSpotifyClient authSpotifyClient) {
+    private final AlbumSpotifyClient albumSpotifyClient;
+
+    public AlbumController(AuthSpotifyClient authSpotifyClient, AlbumSpotifyClient albumSpotifyClient) {
         this.authSpotifyClient = authSpotifyClient;
+        this.albumSpotifyClient = albumSpotifyClient;
     }
 
     @GetMapping("/albums")
-    public ResponseEntity<String> helloWorld() {
+    public ResponseEntity<List<Album>> helloWorld() {
         var request = new LoginRequest("client_credentials",
                 "e7b49745a69b45059d31033e2638e8c1",
                 "1de1a2bc971941218785ddc0ca320c2d");
-        return ResponseEntity.ok(authSpotifyClient.login(request).getAccessToken());
+        var token = authSpotifyClient.login(request).getAccessToken();
+
+        var response =albumSpotifyClient.getReleases("Bearer " + token);
+        return ResponseEntity.ok(response.getAlbums().getItems());
     }
 }
