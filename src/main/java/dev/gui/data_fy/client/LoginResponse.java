64a4c6b44df05e12df2820cbfa0f6f71bb23b6b9
Guilherme Silva <sgsouza.173@gmail.com>
2024-10-17 10:12:28 -0300

    token login funcional

--- src/main/java/dev/gui/data_fy/client/LoginResponse.java
+++ src/main/java/dev/gui/data_fy/client/LoginResponse.java
@@ -1,5 +1,11 @@
 package dev.gui.data_fy.client;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class LoginResponse {
     private String accessToken;
 
--- src/main/java/dev/gui/data_fy/controller/AlbumController.java
+++ src/main/java/dev/gui/data_fy/controller/AlbumController.java
@@ -1,6 +1,8 @@
 package dev.gui.data_fy.controller;
 
+import dev.gui.data_fy.client.AuthSpotifyClient;
 import dev.gui.data_fy.client.GoogleClient;
+import dev.gui.data_fy.client.LoginRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/spotify/api")
 public class AlbumController {
 
-    private final GoogleClient googleClient;
+    private final AuthSpotifyClient authSpotifyClient;
 
-    public AlbumController(GoogleClient googleClient) {
-        this.googleClient = googleClient;
+    public AlbumController(AuthSpotifyClient authSpotifyClient) {
+        this.authSpotifyClient = authSpotifyClient;
     }
 
     @GetMapping("/albums")
     public ResponseEntity<String> helloWorld() {
-        return ResponseEntity.ok(googleClient.helloWorld());
+        var request = new LoginRequest("client_credentials",
+                "e7b49745a69b45059d31033e2638e8c1",
+                "1de1a2bc971941218785ddc0ca320c2d");
+        return ResponseEntity.ok(authSpotifyClient.login(request).getAccessToken());
     }
 }
