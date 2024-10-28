758e38386b75d5a9bd73378956cdbdf414f32085
Guilherme Silva <sgsouza.173@gmail.com>
2024-10-28 15:08:10 -0300

    Alterações no back-end para leitura
    Agora o projeto lê imagens de forma correta, funciona como deveria

--- front-end/src/components/AlbumCard.js
+++ front-end/src/components/AlbumCard.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Card, CardContent, Typography, CardMedia } from '@mui/material';
 
 function AlbumCard({ album }) {
+    if (!album) {
+        album = {};
+    }
+
     return (
         <Card>
             <CardMedia
                 component="img"
                 height="140"
-                image={album.image[0]?.url || 'https://via.placeholder.com/150'}
+                image={album.images[0].url || 'https://via.placeholder.com/150'}
                 alt={album.name}
             />
             <CardContent>
