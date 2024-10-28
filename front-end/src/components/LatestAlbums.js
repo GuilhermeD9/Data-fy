cf9fcb96507e918d4bd416a5ffad67b8d5b1e5c1
Guilherme Silva <sgsouza.173@gmail.com>
2024-10-28 11:11:00 -0300

    Create LatestAlbums.js

---
+++ front-end/src/components/LatestAlbums.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import { Grid2, Container, Typography } from "@mui/material";
+import AlbumCard from './AlbumCard';
+
+function LatestAlbums() {
+    const [albums, setAlbums] = useState([]);
+
+    useEffect(() => {
+        axios.get('spotify/api/albums', {
+            headers: {
+                Authorization: 'Bearer TOKEN_DE_ACESSO',
+            }
+        })
+        .then(response => setAlbums(response.data.items))
+        .catch(error => console.error('Erro ao buscar os albuns:', error));
+    }, []);
+
+    return (
+        <Container>
+            <Typography variant="h4" component="h2" gutterBottom>
+                Últimos Álbuns Lançados
+            </Typography>
+            <Grid2 container spacing={3}>
+                {albums.map(album => (
+                    <Grid2 item xs={12} sm={6} md={4} key={album.id}>
+                        <AlbumCard album={album} />
+                    </Grid2>
+                ))}
+            </Grid2>
+        </Container>
+    );
+}
+
+export default LatestAlbums;
