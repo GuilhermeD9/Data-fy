c0c860434f4d8c613288563bec82d9445745990e
Guilherme Silva <sgsouza.173@gmail.com>
2024-11-06 10:06:09 -0300

    Update DataFyApplicationTests.java

--- back-end/src/test/java/dev/gui/data_fy/DataFyApplicationTests.java
+++ back-end/src/test/java/dev/gui/data_fy/DataFyApplicationTests.java
@@ -1,13 +1,46 @@
 package dev.gui.data_fy;
 
+import dev.gui.data_fy.client.TopPlaylistSpotifyClient;
+import dev.gui.data_fy.service.SpotifyService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class DataFyApplicationTests {
 
+	@Mock
+	private TopPlaylistSpotifyClient topPlaylistSpotifyClient;
+
+	@InjectMocks
+	private SpotifyService spotifyService;
+
+	@MockBean
+	private SpotifyService spotifyServicie2;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+
 	@Test
-	void contextLoads() {
+	void getTopWorldTracks_ShouldHandleErrorGracefully() {
+		// Simula uma exceção da API do Spotify
+		Mockito.when(topPlaylistSpotifyClient.getTopTracksPlaylist(anyString(), anyString(), anyInt()))
+				.thenThrow(new RuntimeException("API error"));
+
+		assertThrows(RuntimeException.class, () -> spotifyService.getTopRegionTracks("dummyToken", "roma"));
 	}
 
 }
